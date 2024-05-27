e27cd4251a8feac9c98d4baf8886217035e2317b
Samandar Baratov <sabaratov9999@gmail.com>
2024-05-27 22:25:27 +0500

    Layers Added

---
+++ ToDoListProject.DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoListProject.Domain.Entity;
+
+namespace ToDoListProject.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<TaskEntity> Tasks { get; set; }
+    }
+}
